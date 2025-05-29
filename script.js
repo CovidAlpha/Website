de94ad0d1823bea875782029d89e63e31e1a1baa
CovidAlpha <mesbahtelegrambots@gmail.com>
2025-05-29 12:51:08 +0330

    ADDED RGB HYPE

---
+++ script.js
@@ -0,0 +1,16 @@
+<!-- Add this before </body> -->
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script>
+  particlesJS("body", {
+    "particles": {
+      "number": { "value": 80 },
+      "color": { "value": ["#ff00fc", "#00fffc", "#ffcc00"] },
+      "shape": { "type": "circle" },
+      "size": { "value": 3, "random": true },
+      "move": { 
+        "speed": 2, 
+        "out_mode": "bounce" 
+      }
+    }
+  });
+</script>
